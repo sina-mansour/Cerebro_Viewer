dc62211b4e26c376c881a091fc147be35780c9cf
jaysonjeg <jayson.jeganathan@gmail.com>
2023-07-21 15:41:21 -0400

    modified plot_dscalar.py

--- docs/examples/simple_surface_rendering/plot_dscalar.py
+++ docs/examples/simple_surface_rendering/plot_dscalar.py
@@ -1,20 +1,24 @@
 """
 ================
-Plot in interactive window and add scalar layer
+Add scalar layer
 ================
-This demo showcases 2 features:
-1) Plot in interactive window instead of on matplotlib axis, so that you can zoom/pan/rotate using the mouse.
-2) Add a dscalar.nii surface map, as a colored layer
+This demo adds a dscalar.nii surface map as a colored layer
 """
 from cerebro import cerebro_brain_utils as cbu
 from cerebro import cerebro_brain_viewer as cbv
 import matplotlib.pyplot as plt
 
-my_brain_viewer = cbv.Cerebro_brain_viewer(offscreen=False)
+my_brain_viewer = cbv.Cerebro_brain_viewer(offscreen=True)
 surface_model = my_brain_viewer.load_template_GIFTI_cortical_surface_models('pial')
 cifti_space = my_brain_viewer.visualize_cifti_space()
 
 dscalar_file = cbu.get_data_file(f'templates/HCP/dscalars/S1200.curvature_MSMAll.32k_fs_LR.dscalar.nii')
 my_brain_viewer.add_cifti_dscalar_layer(dscalar_file=dscalar_file, colormap=plt.cm.inferno, opacity=1)
 
-my_brain_viewer.show()
+fig, ax = plt.subplots(figsize=(10,10))
+ax.axis('off')
+my_brain_viewer.offscreen_draw_to_matplotlib_axes(ax)
+
+# Clear this viewer
+my_brain_viewer.viewer.window.destroy()
+plt.show()
